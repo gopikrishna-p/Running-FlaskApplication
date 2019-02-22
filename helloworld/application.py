fc7a534fe30dc69b4bfbd039df13afcaf1951ece
Gopikrishna <gopikrishnapavuluri23@gmail.com>
2019-02-23 00:32:44 +0530

    Update application.py
    Added service

--- helloworld/application.py
+++ helloworld/application.py
@@ -13,6 +13,10 @@ def get():
 def hello():
     return Response(json.dumps({'Output': 'my first flask application'}), mimetype='application/json', status=200)
 
+@application.route('/hella')
+def hella():
+    return Response(json.dumps({'Output': 'my first flask application'}), mimetype='application/json', status=200)
+
 
 @application.route('/', methods=['POST'])
 def post():
